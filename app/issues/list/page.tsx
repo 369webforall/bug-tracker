c90efda5883d272ca08cce0ca0fc873140a0d5a7
369webforall <369webforall@gmail.com>
2023-10-28 21:05:38 +0545

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,9 +3,19 @@ import prisma from '@/prisma/client';
 import { Link, IssueStatusBadge } from '@/app/components';
 
 import IssueActions from './IssueActions';
+import { Status } from '@prisma/client';
+interface Props {
+  searchParams: { status: Status };
+}
 
-const IssuePage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuePage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
 
   return (
     <div>
