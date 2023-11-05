bbe5d144caa7cb1fa28fbfe6b39a79341167351c
369webforall <369webforall@gmail.com>
2023-11-05 19:35:51 +0545

    prisma update

--- prisma/vercel-build.js
+++
@@ -1,7 +0,0 @@
-const { spawnSync } = require('child_process');
-
-// Run Prisma schema generation during the build process
-spawnSync('npx', ['prisma', 'generate'], { stdio: 'inherit' });
-
-// Start your Next.js application
-require('next');
