aa8407acfac685e78003b09380aa17bc5b129c51
369webforall <369webforall@gmail.com>
2023-11-05 17:34:38 +0545

    update issue staus

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,17 +1,21 @@
 'use client';
+
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
+
 const statuses: { label: string; value?: Status }[] = [
   { label: 'All' },
   { label: 'Open', value: 'OPEN' },
-  { label: 'In-Progress', value: 'IN_PROGRESS' },
+  { label: 'In Progress', value: 'IN_PROGRESS' },
   { label: 'Closed', value: 'CLOSE' },
 ];
+
 const IssueStatusFilter = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
+
   return (
     <Select.Root
       defaultValue={searchParams.get('status') || ''}
@@ -20,6 +24,7 @@ const IssueStatusFilter = () => {
         if (status) params.append('status', status);
         if (searchParams.get('orderBy'))
           params.append('orderBy', searchParams.get('orderBy')!);
+
         const query = params.size ? '?' + params.toString() : '';
         router.push('/issues/list' + query);
       }}
@@ -27,7 +32,7 @@ const IssueStatusFilter = () => {
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value || ''}>
+          <Select.Item key={status.value} value={status.value!}>
             {status.label}
           </Select.Item>
         ))}
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,28 +1,83 @@
-import { columnNames } from './IssueTable';
-import { IssueQuery } from './IssueTable';
-import prisma from '@/prisma/client';
+// import { columnNames } from './IssueTable';
+// import { IssueQuery } from './IssueTable';
+// import prisma from '@/prisma/client';
 
-import IssueActions from './IssueActions';
-import { Status } from '@prisma/client';
+// import IssueActions from './IssueActions';
+// import { Status } from '@prisma/client';
 
+// import Pagination from '@/app/components/Pagination';
+// import IssueTable from './IssueTable';
+// import { Flex } from '@radix-ui/themes';
+// import type { Metadata } from 'next';
+// interface Props {
+//   searchParams: IssueQuery;
+// }
+
+// const IssuePage = async ({ searchParams }: Props) => {
+//   const statuses = Object.values(Status);
+//   const status = statuses.includes(searchParams.status)
+//     ? searchParams.status
+//     : undefined;
+
+//   const orderBy = columnNames.includes(searchParams.orderBy)
+//     ? { [searchParams.orderBy]: 'asc' }
+//     : undefined;
+//   const where = { status };
+//   const page = parseInt(searchParams.page) || 1;
+//   const pageSize = 10;
+
+//   const issues = await prisma.issue.findMany({
+//     where,
+//     orderBy,
+//     skip: (page - 1) * pageSize,
+//     take: pageSize,
+//   });
+//   const issueCount = await prisma.issue.count({ where });
+
+//   return (
+//     <Flex direction="column" gap="3">
+//       <IssueActions />
+//       <IssueTable searchParams={searchParams} issues={issues} />
+//       <Pagination
+//         pageSize={pageSize}
+//         currentPage={page}
+//         itemCount={issueCount}
+//       />
+//     </Flex>
+//   );
+// };
+
+// export const dynamic = 'force-dynamic';
+
+// export default IssuePage;
+
+// export const metadata: Metadata = {
+//   title: 'Issue Tracker - Issue List',
+//   description: 'View all project issues',
+// };
 import Pagination from '@/app/components/Pagination';
-import IssueTable from './IssueTable';
+import prisma from '@/prisma/client';
+import { Status } from '@prisma/client';
+import IssueActions from './IssueActions';
+import IssueTable, { IssueQuery, columnNames } from './IssueTable';
 import { Flex } from '@radix-ui/themes';
-import type { Metadata } from 'next';
+import { Metadata } from 'next';
+
 interface Props {
   searchParams: IssueQuery;
 }
 
-const IssuePage = async ({ searchParams }: Props) => {
+const IssuesPage = async ({ searchParams }: Props) => {
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const where = { status };
 
   const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: 'asc' }
     : undefined;
-  const where = { status };
+
   const page = parseInt(searchParams.page) || 1;
   const pageSize = 10;
 
@@ -32,6 +87,7 @@ const IssuePage = async ({ searchParams }: Props) => {
     skip: (page - 1) * pageSize,
     take: pageSize,
   });
+
   const issueCount = await prisma.issue.count({ where });
 
   return (
@@ -49,9 +105,9 @@ const IssuePage = async ({ searchParams }: Props) => {
 
 export const dynamic = 'force-dynamic';
 
-export default IssuePage;
-
 export const metadata: Metadata = {
   title: 'Issue Tracker - Issue List',
   description: 'View all project issues',
 };
+
+export default IssuesPage;
