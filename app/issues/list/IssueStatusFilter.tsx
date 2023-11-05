850d7680dd0dd291434c4486a8b1e0d00ef5624a
369webforall <369webforall@gmail.com>
2023-11-05 15:43:32 +0545

    update

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -27,7 +27,7 @@ const IssueStatusFilter = () => {
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.label} value={status.value}>
+          <Select.Item key={status.value} value={status.value || ''}>
             {status.label}
           </Select.Item>
         ))}
